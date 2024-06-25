e6fc6c69f4af36ad56a5b643d8cbf235f959b65b
DisonUyoga <disonobudho233@gmail.com>
2024-06-25 12:12:28 +0300

    build

--- babel.config.js
+++ babel.config.js
@@ -3,11 +3,14 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      "babel-preset-expo","module:metro-react-native-babel-preset",
+      "babel-preset-expo",
       ["@babel/preset-env", { targets: { node: "current" } }],
       "@babel/preset-typescript",
     ],
-    plugins: ["nativewind/babel", "react-native-reanimated/plugin","@babel/plugin-transform-runtime"],
-    
+    plugins: [
+      "nativewind/babel",
+      "react-native-reanimated/plugin",
+      "@babel/plugin-transform-runtime",
+    ],
   };
 };
